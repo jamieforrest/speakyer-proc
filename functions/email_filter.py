1ebdca9492399f7e58f437ea887853ba1af097ff
Jamie Forrest <jamie.forrest@coursemojo.com>
2024-09-07 18:58:17 -0400

    Invoke lambda

--- functions/email_filter.py
+++ functions/email_filter.py
@@ -1,10 +1,15 @@
 import boto3  # type: ignore
+import logging
 import json
 import os
 from models import EmailInfo
 from responses import error_response, forbidden_response, success_response
+from typing import Any, Dict
 
 s3 = boto3.client("s3")
+lambda_client = boto3.client("lambda")
+
+logger = logging.getLogger()
 
 
 def validate_sender(sender: str) -> bool:
@@ -12,6 +17,18 @@ def validate_sender(sender: str) -> bool:
     return sender.lower() in [
         email.lower() for email in os.environ["WHITELIST"].split(",")
     ]
+
+
+def invoke_next_lambda(payload: Dict[str, Any]) -> None:
+    function_name = os.environ["NEXT_LAMBDA"]
+    logger.info(f"Invoking {function_name} with payload: {payload}")
+    # invoke the next lambda synchronously
+    # response = lambda_client.invoke(
+    #     FunctionName=function_name,
+    #     InvocationType="RequestResponse",
+    #     Payload=json.dumps(payload),
+    # )
+    logger.info(f"Response from {function_name}: {hi}")
 
 
 def lambda_handler(event, _):
@@ -33,6 +50,8 @@ def lambda_handler(event, _):
                 Key=key,
                 Body=json.dumps(email_info.message),
             )
+            payload = {"bucket": bucket, "key": key, "sender": sender}
+            invoke_next_lambda(payload)
             return success_response(f"Email from {sender} stored.")
         except Exception as e:
             # TODO alert sender of error
