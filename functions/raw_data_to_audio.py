5b323618e1636de7ae5e25ba5e2c5dd57ebc4080
Jamie Forrest <jamie.forrest@coursemojo.com>
2024-09-08 00:03:29 -0400

    Split text into chunks for TTS, send them out in parallel, and then reassemble

--- functions/raw_data_to_audio.py
+++ functions/raw_data_to_audio.py
@@ -4,13 +4,18 @@ import os
 import tempfile
 
 from botocore.exceptions import ClientError  # type: ignore
+from concurrent.futures import ThreadPoolExecutor, as_completed
+from natsort import natsorted
 from openai import OpenAI
+from pydub import AudioSegment  # type: ignore
 from responses import success_response, error_response
-from typing import Literal
+from typing import List, Literal
 
 s3 = boto3.client("s3")
 logger = logging.getLogger()
 openai_client = OpenAI(api_key=os.environ["OPENAI_API_KEY"], timeout=60)
+
+MAX_TTS_CHARS = 4096
 
 
 def get_file_content_from_s3(bucket: str, key: str) -> str:
@@ -48,13 +53,24 @@ def extract_text_from_raw_data(raw_data: str) -> str | None:
     return extracted_text
 
 
+def reassemble_audio_files(tmp_files: List[str]) -> str:
+    """Reassemble the audio files into one."""
+    audio_segments = [AudioSegment.from_file(file) for file in tmp_files]
+    combined = sum(audio_segments)
+    temp_dir = tempfile.gettempdir()
+    speech_file_path = os.path.join(temp_dir, "speech.mp3")
+    combined.export(speech_file_path, format="mp3")
+    return speech_file_path
+
+
 def text_to_audio(
     text: str,
+    filename: str,
     voice: Literal["alloy", "echo", "fable", "onyx", "nova", "shimmer"] = "alloy",
 ) -> str:
     """Convert text to audio. Returns the path to the audio file."""
     temp_dir = tempfile.gettempdir()
-    speech_file_path = os.path.join(temp_dir, "speech.mp3")
+    speech_file_path = os.path.join(temp_dir, filename + ".mp3")
     with openai_client.audio.speech.with_streaming_response.create(
         model="tts-1",
         voice=voice,
@@ -62,6 +78,20 @@ def text_to_audio(
     ) as response:
         response.stream_to_file(speech_file_path)
     return speech_file_path
+
+
+def chunk_texts(text: str, max_length: int = MAX_TTS_CHARS) -> List[str]:
+    chunks = text.split("\n")
+    chunks_to_return = []
+    for chunk in chunks:
+        if len(chunk) > max_length:
+            # If the chunk is too long, raise an error
+            # TODO: Handle this more gracefully
+            raise ValueError("Text chunk is too long")
+        if chunk:
+            # Only add non-empty chunks
+            chunks_to_return.append(chunk)
+    return chunks_to_return
 
 
 def file_exists(bucket: str, key: str) -> bool:
@@ -118,6 +148,9 @@ def lambda_handler(event, _):
     if not text_file_exists:
         extracted_text = extract_text_from_raw_data(raw_data)
     else:
+        logger.info(
+            f"Text file already exists: {output_bucket}/{text_key}, getting text from S3"
+        )
         extracted_text = get_file_content_from_s3(output_bucket, text_key)
 
     if not text_file_exists and extracted_text:
@@ -130,8 +163,23 @@ def lambda_handler(event, _):
     audio_key = new_key_for_processed_file(key, "audios", "mp3")
     audio_file_exists = file_exists(output_bucket, audio_key)
     if not audio_file_exists:
-        tmp_file = text_to_audio(extracted_text)
+        # Chunk the text into smaller pieces
+        chunked_texts = chunk_texts(extracted_text)
+
+        # Convert each chunk to audio in parallel
+        futures = []
+        chunk_files = []
+        with ThreadPoolExecutor() as executor:
+            for idx, chunk in enumerate(chunked_texts):
+                futures.append(executor.submit(text_to_audio, chunk, str(idx), "alloy"))
+
+            chunk_files = [future.result() for future in as_completed(futures)]
+
+        # Reassemble the audio files and write the final audio file to S3
+        tmp_file = reassemble_audio_files(natsorted(chunk_files))
         write_file_to_s3(output_bucket, audio_key, tmp_file)
+    else:
+        logger.info(f"Audio file already exists: {output_bucket}/{audio_key}")
 
     return success_response(
         f"Extracted text from {key} stored in {output_bucket}/{text_key} and audio stored in {output_bucket}/{audio_key}"
